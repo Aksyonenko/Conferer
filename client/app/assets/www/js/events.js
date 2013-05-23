42f62452efae448d2a90687a133f2f181a2318e8
Yuriy Belelya <Yuriy.Belelya@akqa.com>
2013-05-23 16:22:16 +0300

    Repo cleanup

--- client/app/assets/www/js/events.js
+++
@@ -1,5 +0,0 @@
-var conferer = conferer || {};
-conferer.events = {};
-
-conferer.events.conferenceList = {};
-_.extend(conferer.events.conferenceList, Backbone.Events);
--- client/app/assets/www/js/run.js
+++
@@ -1,1 +0,0 @@
-coferer.initialize();
