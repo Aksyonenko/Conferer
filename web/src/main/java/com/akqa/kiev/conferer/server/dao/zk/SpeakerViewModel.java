683c032053024bf9527f73f457b03ee69da4b020
natalia.bovda <Natalia.Bovda@akqa.com>
2013-08-06 19:07:21 +0300

    Conferences admin page added

--- web/src/main/java/com/akqa/kiev/conferer/server/dao/zk/SpeakerViewModel.java
+++ web/src/main/java/com/akqa/kiev/conferer/server/dao/zk/SpeakerViewModel.java
@@ -3,7 +3,6 @@ package com.akqa.kiev.conferer.server.dao.zk;
 import org.zkoss.bind.annotation.Init;
 
 import com.akqa.kiev.conferer.server.dao.SpeakerDao;
-import com.akqa.kiev.conferer.server.model.SocialLinks;
 import com.akqa.kiev.conferer.server.model.Speaker;
 
 public class SpeakerViewModel extends AbstractEntityListModel<Speaker> {
