ce081e5ccaa1973f7134cf82eece384208eb7604
Nickolay <nickolay.polozov@akqa.com>
2013-11-22 15:50:10 +0200

    Add up navigation caret to ConferenceDetailsActivity

--- client/android/AKQAConferer/src/com/akqa/kiev/android/conferer/ConferenceDetailActivity.java
+++ client/android/AKQAConferer/src/com/akqa/kiev/android/conferer/ConferenceDetailActivity.java
@@ -4,6 +4,8 @@ import com.akqa.kiev.android.conferer.web.ConfererService;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 
 public class ConferenceDetailActivity extends FragmentActivity {
 	
@@ -12,6 +14,18 @@ public class ConferenceDetailActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_conference_details);
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 }
